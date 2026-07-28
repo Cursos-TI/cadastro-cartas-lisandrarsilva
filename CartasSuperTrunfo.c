0fffbb2c9362de1c8eb14cd0138804183e6d8ff4
lisandrarsilva <lisandra.rsilva@gmail.com>
2026-07-28 15:58:59 -0300

    Implement user input for city card properties
    Added user input for city properties including state, code, population, area, GDP, and tourist points.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,43 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+char estado[1];
+    int codigo;
+    char cidade[30];
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
   // Área para entrada de dados
+ printf("Insira uma letra de A a H:\n");
+    scanf("%s", estado);
 
+    printf("Insira um código de 01 a 04:\n");
+    scanf("%d", &codigo);
+
+    printf("Insira sua cidade:\n");
+    scanf("%s", cidade);
+
+    printf("Insira a população:\n");
+    scanf("%d", &populacao);
+
+    printf("Insira a área da sua cidade:\n");
+    scanf("%f", &area);
+
+    printf("Insira o PIB da sua cidade:\n");
+    scanf("%f", &pib);
+
+    printf("Insira o número de pontos turísticos da sua cidade:\n");
+    scanf("%d", &pontos_turisticos);
   // Área para exibição dos dados da cidade
+printf("Carta %d\n", codigo);
+printf("Estado: %s\n", estado);
+printf("Código: %s %d\n", estado, codigo);
+printf("Nome da Cidade: %s\n", cidade);
+printf("População: %d\n", populacao);  
+printf("Área: %.2f km²\n", area);
+printf("PIB: %.2f bilhões de reais\n", pib);
+printf("Pontos Turísticos: %d\n", pontos_turisticos);
 
 return 0;
 } 
